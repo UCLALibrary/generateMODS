8965e011be34b7b3a9138da693a6c0358b50be4d
pghorpade <pghorpade@library.ucla.edu>
2021-03-05 18:05:58 -0800

    Disable not implemented tests and add display names to the test and a maven  plugin

--- src/test/java/edu/ucla/library/dep/GenerateMods/OpenCsvIdepTest.java
+++ src/test/java/edu/ucla/library/dep/GenerateMods/OpenCsvIdepTest.java
@@ -5,35 +5,49 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OpenCsvIdepTest {
 
 	@Test
+	@DisplayName("Create Subject")
+	@Disabled
 	void testCreateSubjectElement() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@DisplayName("Create Date")
+	@Disabled
 	void testCreateDateElement() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@DisplayName("Create Language")
+	@Disabled
 	void testCreateLanguage() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@DisplayName("Create Related Element")
+	@Disabled
 	void testCreateRelatedItemElement() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@DisplayName("Create Element")
+	@Disabled
 	void testCreateElementFromString() {
 		fail("Not yet implemented");
 	}
+	
 	@Test
+	@DisplayName("Get User Input")
 	void testGetInput() {
 
 	    String input = "MEAP";
@@ -45,6 +59,8 @@ class OpenCsvIdepTest {
 	}
 
 	@Test
+	@DisplayName("Main")
+	@Disabled
 	void testMain() {
 		fail("Not yet implemented");
 	}
